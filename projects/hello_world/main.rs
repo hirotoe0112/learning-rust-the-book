7b549f58ad1a96b30b523c046b1f1d4a4a0cd217
hirotoe0112 <hirotoe0112@gmail.com>
2026-01-15 21:46:14 +0000

    first hello world

---
+++ projects/hello_world/main.rs
@@ -0,0 +1,11 @@
+/**
+ * main function
+ * The first code that runs in every executable program.
+ * Can contain parameters.
+ */
+fn main() {
+    // Calls a Rust macro
+    // If it calls a normal function, use println without !
+    // see: Chapter 20
+    println!("Hello, world!");
+}
